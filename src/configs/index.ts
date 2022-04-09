d7ffe93036ba5db742b2d81190285d9fe55edaba
Joyce Ribeiro <riber.joy@gmail.com>
2022-04-09 11:50:51 -0300

    Criacao da pagina star wars e a busda por id da mesma

---
+++ src/configs/index.ts
@@ -0,0 +1,10 @@
+export const configs = {
+    apis: {
+        cep: process.env.REACT_APP_CEP_URL,
+        starWars: process.env.REACT_APP_STAR_WARS_BASE_URL, 
+        github: process.env.REACT_APP_GITHUB_URL,
+    },
+    sentry:     "https://2bebeba9f2bd42cf99d43bad17da69e3@o1196585.ingest.sentry.io/6319599",
+}
+
+export default configs;
--- src/containers/combustivel/index.tsx
+++ src/containers/combustivel/index.tsx
@@ -1,69 +1,60 @@
 import * as React from 'react';
-
 import { Container, Grid, Header, Form, Button } from 'semantic-ui-react';
 import { inject, observer } from 'mobx-react';
-
+import NewRouterStore from '../../mobx/router.store';
 import CombustivelStore from './store';
 
 interface Props {
-    combustivel: CombustivelStore
+  router: NewRouterStore;
+  combustivel: CombustivelStore;
 }
 
-@inject('combustivel')
+@inject('router', 'combustivel')
 @observer
-export default class Combustivel extends React.Component<Props>{
+export default class Combustivel extends React.Component<Props> {
 
-    render() {
+  render() {
 
-        const { etanol, gasolina, submit, handleForm } = this.props.combustivel;
+    const { etanol, gasolina, submit, handleForm } = this.props.combustivel;
 
-        const submitForm = (e) => {
-            e.preventDefault();
-            submit();
-        }
-
-        return (
-            <Container>
-                <Grid divided='vertically'>
-                    <Grid.Row columns={2}>
-                        <Grid.Column>
-                            <Header color='blue' as='h2'>
-                                <Header.Content>
-                                    Combustível
-                                    <Header.Subheader>
-                                        Etanol ou Gasolina?
-                                    </Header.Subheader>
-                                </Header.Content>
-                            </Header>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-                <Form onSubmit={submitForm}>
-                    <Form.Group widths='equal'>
-                        <Form.Field>
-                            <label>Preço da Gasolina</label>
-                            <input step='any'
-                                max='99'
-                                value={gasolina}
-                                onChange={handleForm}
-                                name='gasolina'
-                                type='number'
-                                placeholder='ex R$ 4.05' />
-                        </Form.Field>
-                        <Form.Field>
-                            <label>Preço do Etanol</label>
-                            <input step='any'
-                                max='99'
-                                value={etanol}
-                                onChange={handleForm}
-                                name='etanol'
-                                type='number'
-                                placeholder='ex R$ 2.00' />
-                        </Form.Field>
-                    </Form.Group>
-                    <Button type='submit'>Consultar</Button>
-                </Form>
-            </Container>
-        );
+    const submitForm = (e) => {
+      e.preventDefault();
+      submit();
     }
+
+    const breakCode = () => {
+        throw new Error('Erroooo')
+    }
+
+    return (
+      <Container>
+        <Grid divided='vertically'>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Header color='blue' as='h2'>
+                <Header.Content>
+                  Combustível
+                 <Header.Subheader>Alcool ou Gasolina?</Header.Subheader>
+                </Header.Content>
+              </Header>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Form onSubmit={submitForm}>
+          <Form.Group widths='equal'>
+            <Form.Field>
+              <label>Preço da Gasolina</label>
+              <input step="any" max="99" value={gasolina} name='gasolina' onChange={handleForm} type='number' placeholder='ex: R$ 4.05' />
+            </Form.Field>
+            <Form.Field>
+              <label>Preço do Etanol</label>
+              <input step="any" max="99" value={etanol} name='etanol' onChange={handleForm} type='number' placeholder='ex: R$ 2.00' />
+            </Form.Field>
+          </Form.Group>
+          <button type='button' onClick={breakCode}>Break the world</button>
+          <Button type='submit'>Consultar</Button>
+        </Form>
+      </Container>
+    );
+  }
 }
